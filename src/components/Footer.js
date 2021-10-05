c3cdea9e664a252d7a8f8810e610c8c3d029a848
Ankit Sangwan <44750718+ankitsangwan1999@users.noreply.github.com>
2021-10-06 02:01:28 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "../styles/Footer.css";
 
 // TODO: ISSUE: Make Footer more appealing. +5 fo Creativity.
 const Footer = () => {
-    return <div className="Footer">Made with love by @ankit.sangwan1999</div>;
+    return <div className="Footer">Made with peace for ContriHUB-21</div>;
 };
 
 export default Footer;
