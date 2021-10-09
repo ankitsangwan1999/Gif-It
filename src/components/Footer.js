7f1955d1280880bd53df805f7dc83c173b332049
jitendra bairwa <92137204+jitendra-bairwa@users.noreply.github.com>
2021-10-09 16:23:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import "../styles/Footer.css";
-
 // TODO: ISSUE: Make Footer more appealing. +5 fo Creativity.
 const Footer = () => {
-    return <div className="Footer">Made with peace for ContriHUB-21</div>;
+    return (
+        <footer class="footer">
+            <div class="outer-footer">
+                <a href="https://contrihub.github.io/"> Made with peace for ContriHUB-21</a>
+            </div>
+        </footer>
+    );
 };
-
 export default Footer;
