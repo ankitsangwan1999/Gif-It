4994167ac50632e33c15efa77cbfa9553760a177
overlord-ak <overlord.1912.ak@gmail.com>
2021-10-16 12:21:46 +0530

    Fixed thumbnails issue

--- src/components/VideoSuggestions.js
+++ src/components/VideoSuggestions.js
@@ -1,15 +1,24 @@
-import React from "react";
-import VideoListItem from "./VideoListItem";
-import "../styles/VideoSuggestions.css";
+import React from 'react';
+import VideoListItem from './VideoListItem';
+import '../styles/VideoSuggestions.css';
 
-const VideoSuggestions = ({ videosList }) => {
-    return (
-        <div className="VideoSuggestions">
-            {videosList.map((video) => (
-                <VideoListItem key={video.etag} videoItem={video} />
-            ))}
-        </div>
-    );
+const VideoSuggestions = ({ videosList, setVideoDetails }) => {
+  return (
+    <div className="VideoSuggestions">
+      {videosList.map((video, index) => {
+        return index > 0 ? (
+          <VideoListItem
+            key={video.etag}
+            videoItem={video}
+            setVideoDetails={setVideoDetails}
+            thumbnail={video.snippet.thumbnails.default.url}
+          />
+        ) : (
+          <div></div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default VideoSuggestions;
