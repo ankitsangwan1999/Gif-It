3325b2d417d5675799839ed3cdaeed1ee9917ac5
ankit.sangwan1999 <ankit.sangwan1999@gmail.com>
2021-11-16 02:09:33 +0530

    Added Clock Icons

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+
 const Progress = ({ messages }) => {
+    const clocks = ["🕛", "🕒", "🕕", "🕤"];
+    const [clock, setClock] = useState("🕛");
+    useEffect(() => {
+        let i = 0;
+        const interval = setInterval(() => {
+            setClock(`${clocks[i]}`);
+            ++i;
+            if (i === 4) i = 0;
+        }, 200);
+        return () => {
+            clearInterval(interval);
+            console.log("Log: Interval Cleared");
+        };
+    }, []);
+
     return (
         <div className="Progress">
             {messages.map((message, index) =>
                 index === messages.length - 1 ? (
-                    <p key={index}>&#x23F2; {message}</p>
+                    <p key={index}>
+                        <span>{clock}</span> {message}
+                    </p>
                 ) : (
                     <p key={index}>&#x2705; {message}</p>
                 )
