65376498dd0174dd63fe43a98e95839e892f7ecb
Ankit Sangwan <ankit-sangwan@Ankits-MacBook-Air.local>
2025-03-22 19:00:50 +0530

    Add Referer

--- server/helpers/makeGif.mjs
+++ server/helpers/makeGif.mjs
@@ -29,6 +29,7 @@ const getSourceUrl = async (watchUrl) => {
             preferFreeFormats: true,
             youtubeSkipDashManifest: true,
             noCacheDir: true,
+            referer: watchUrl
         });
         const sourceUrl = response.url;
 
